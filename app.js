57f95303c9e77bf0840e29611b00e1b1b0ad4734
saikiranreddy-16 <chirrasaikiranreddy16@gmail.com>
2026-06-27 22:46:18 +0530

    Add listing model

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express=require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("../model/Lisitng.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/test"
+const MONGO_URL = "mongodb://127.0.0.1:27017/test";
 
 main()
     .then(()=>{
@@ -18,6 +19,20 @@ async function main(){
 
 app.get("/",(req,res)=>{
     res.send("This is root");
+});
+
+app.get("/testListing",(req,res)=>{
+    let sampleListing = new Listing({
+        title: "My new Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "calangute, Goa",
+        country: "India",
+    });
+
+await sampleListing.save();
+console.log("sample was saved");
+res.send("successful testing");
 });
 app.listen(3030,()=>{
     console.log("server is listening to port 3030");
